e1f7e866eff3bb82beb0116352b631622208c399
Elsadiq <you@example.com>
2019-11-22 15:27:54 +0100

    change:
    changed the port number

--- ims-comments/buildAndRun.sh
+++ ims-comments/buildAndRun.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mvn clean package && docker build -t org.digam/ims-comments .
-docker run -p 8081:8080 --name ims-comments org.digam/ims-comments
+docker run -p 8083:8080 --name ims-comments org.digam/ims-comments
